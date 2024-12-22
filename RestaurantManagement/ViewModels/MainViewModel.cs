8f34c69bb333f9f6c4b3d0a5031329991e8c8543
Thinh <heyiamthinh1003@gmail.com>
2024-12-22 14:58:16 +0700

    kcj

--- RestaurantManagement/ViewModels/MainViewModel.cs
+++ RestaurantManagement/ViewModels/MainViewModel.cs
@@ -95,14 +95,15 @@ namespace RestaurantManagement.ViewModels
 
 
             // Xử lý logic khi đăng xuất
-            LogOutCommand = new RelayCommand<Window>(
-                (p) => true, // Điều kiện có thể thực thi
-                (p) =>
+            LogOutCommand = new RelayCommand<Window>((p) => true, (p) =>
+            {
+                if (p == null)
                 {
-                    MessageBox.Show("Đăng xuất thành công!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Information);
-                    // Thêm logic điều hướng hoặc đóng ứng dụng tại đây
-                    p.Close();
-                });
+                    return;
+                }
+                System.Windows.Forms.Application.Restart();
+                p.Close();
+            });
         }
     }
 }
